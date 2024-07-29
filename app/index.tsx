bd06a780e0fa11d135c33da9e7bd874168110374
Adnan Saleem <adnan13893@gmail.com>
2024-07-30 00:00:33 +0500

    fixed scrollview bug

--- app/index.tsx
+++ app/index.tsx
@@ -7,63 +7,58 @@ import {SafeAreaView} from 'react-native-safe-area-context'
 
 export default function App() {
   return (
-    <SafeAreaView className="h-full bg-primary w-full">
-      <StatusBar hidden />
+    <SafeAreaView className="h-full bg-primary">
       <ScrollView
-        className="gap-y-4"
         contentContainerStyle={{
-          alignItems: 'center',
-          justifyContent: 'center',
-          display: 'flex',
-          flex: 1,
-          height: '100%',
-          width: '100%',
-          padding: 16
+          height: '100%'
         }}
       >
-        {/* Section 1 */}
-        <View className="flex-1/7 items-center justify-center">
-          <Text className="text-3xl font-bold text-white">
-            Reel
-            <Text className="text-blue-400">Mind</Text>
-          </Text>
-        </View>
-        {/* Section 2 */}
-        <View className="flex-3/7 items-center justify-center w-full">
-          <Image
-            source={images.cards}
-            className="h-96 w-auto"
-            resizeMode="contain"
-            resizeMethod="resize"
-          />
-        </View>
-        {/* Section 3 */}
-        <View className="flex-3/7 items-center justify-center w-full gap-y-6">
-          <View className="flex-1/7 items-center justify-center w-full">
-            <Text className="text-3xl font-bold text-center text-white">
-              Discover Endless Possibilities with {/* <View className=""> */}
-              <Text className="text-2xl text-center text-secondary-100">{APP_NAME}</Text>
+        <View className="w-full justify-center items-center h-full px-6 gap-y-4">
+          {/* Section 1 */}
+          <View className="items-center justify-center">
+            <Text className="text-3xl font-bold text-white">
+              Reel
+              <Text className="text-blue-400">Mind</Text>
             </Text>
+          </View>
+          {/* Section 2 */}
+          <View className="items-center justify-center w-full">
             <Image
-              source={images.path}
-              className="w-[100px] h-[15px] absolute right-3 -bottom-1.5"
-              resizeMode="cover"
+              source={images.cards}
+              className="h-96 w-auto"
+              resizeMode="contain"
+              resizeMethod="resize"
             />
           </View>
-          {/* Section 4 */}
-          <View className="flex-1/7 items-center justify-center w-full">
-            <Text className="text-white text-center text-[16px] leading-6">
-              Where Creativity Meets Innovation: Embark on a Journey of Limitless Exploration with{' '}
-              {APP_NAME}
-            </Text>
-          </View>
-          {/* Section 5 */}
-          <View className="flex-1/7 block items-center justify-center w-full">
-            <PrimaryButton
-              text={`Continue to ${APP_NAME}`}
-              onPress={() => router.navigate('/log-in')}
-              // Add Icon Prop
-            />
+          {/* Section 3 */}
+          <View className="items-center justify-center w-full gap-y-6">
+            {/* Bold Highlight Text */}
+            <View className="items-center justify-center w-full">
+              <Text className="text-3xl font-bold text-center text-white">
+                Discover Endless Possibilities with {/* <View className=""> */}
+                <Text className="text-2xl text-center text-secondary-100">{APP_NAME}</Text>
+              </Text>
+              <Image
+                source={images.path}
+                className="w-[100px] h-[15px] absolute right-3 -bottom-1.5"
+                resizeMode="cover"
+              />
+            </View>
+            {/* Small Descriptive Text */}
+            <View className="items-center justify-center w-full">
+              <Text className="text-white text-center text-[16px] leading-6">
+                Where Creativity Meets Innovation: Embark on a Journey of Limitless Exploration with{' '}
+                {APP_NAME}
+              </Text>
+            </View>
+            {/* Button */}
+            <View className="block items-center justify-center w-full">
+              <PrimaryButton
+                text={`Continue to ${APP_NAME}`}
+                onPress={() => router.navigate('/log-in')}
+                // Add Icon Prop
+              />
+            </View>
           </View>
         </View>
       </ScrollView>
