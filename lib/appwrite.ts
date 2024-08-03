c1e111d74a32faa9a36f4da1fd8d789b0def0316
Adnan Saleem <adnan13893@gmail.com>
2024-08-03 22:34:38 +0500

    appwrite configuration added

---
+++ lib/appwrite.ts
@@ -0,0 +1,9 @@
+export const appwriteConfig = {
+  endpoint: 'https://cloud.appwrite.io/v1',
+  platform: 'com.dani.reelforge',
+  projectId: '66ad14aa0006a68332d1',
+  databaseId: '66ae673600118bd61edb',
+  userCollectionId: '66ae674f001ba6fa1325',
+  videoCollectionId: '66ae67650002b7c9154d',
+  storageId: '66ae69dd001ce29d4cfb'
+}
